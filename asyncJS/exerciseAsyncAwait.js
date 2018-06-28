fb0fe2137cc3b16ac8b97e198bc06b9f43ab8669
Joshua Langer <joshuarllanger@gmail.com>
2018-06-28 07:42:14 -0500

    updated asyncJS

--- asyncJS/exerciseAsyncAwait.js
+++ asyncJS/exerciseAsyncAwait.js
@@ -1,10 +1,12 @@
 // Solve the below problems:
 
 // #1) Convert the below promise into async await
-fetch('https://swapi.co/api/starships/9/')
-  .then(response => response.json())
-  .then(console.log)
-
+async function getStarship() {
+  const resp = await fetch('https://swapi.co/api/starships/9/');
+  const starship = await resp.json();
+  console.log(starship);
+}
+getStarship();
 
 // #2) ADVANCED: Update the function below from the video to also have
 // async await for this line: fetch(url).then(resp => resp.json())
@@ -17,13 +19,30 @@ const urls = [
 ]
 
 const getData = async function() {
-  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
-      fetch(url).then(resp => resp.json())
-  ));
-  console.log('users', users);
-  console.log('posta', posts);
-  console.log('albums', albums);
+  try {
+    const [ users, posts, albums ] = await Promise.all(urls.map(
+      async function(url) {
+        const resp = await fetch(url);
+        return resp.json();
+      }
+    ));
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+  } catch (err){
+    console.log('boooo');
+  }
 }
 
 // #3) Add a try catch block to the #2 solution in order to catch any errors.
 // Now chnage one of the urls so you console.log your error with 'ooooooops'
+
+async function getPokemon(id) {
+  try {
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      const pokemon = await resp.json();
+      console.log(pokemon.name);
+    } catch (error){
+    console.log('Why no pokemon?', error);
+  }
+}
