f003948fac7df322a24407130917a59c093acf72
Joshua Langer <joshuarllanger@gmail.com>
2018-07-05 16:14:24 -0500

    update

--- smartbrainapi/server.js
+++ smartbrainapi/server.js
@@ -1,94 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const db = require('knex')({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'MfC1Gp05t',
+		database: 'smartbrain-db'
+	}
+});
 
 const app = express();
 
-/* TODO - Update login with hash, then check with database */
-
 /* MIDDLEWARE */
 app.use(bodyParser.json());
-
-let idCount = 300;
-const database = {
-	users: [
-		{
-			id: '100',
-			name: 'BillyBob',
-			email: 'billybob@gmail.com',
-			entries: 0,
-			joined: new Date(),
-		},
-		{
-			id: '200',
-			name: 'Jean',
-			email: 'jean@yahoo.com',
-			entries: 0,
-			joined: new Date(),
-		},
-		{
-			id: '300',
-			name: 'Paul',
-			email: 'paul@hotmail.com',
-			entries: 0,
-			joined: new Date(),
-		},
-	],
-	login: [
-		{
-			id: '100',
-			hash: '',
-			email: 'billybob@gmail.com'
-		}
-	]
-}
-function searchDatabase(req, res) {
-	
-}
+app.use(cors());
 
 /* ROUTES */
 app.get('/', (req, res) => {
-	res.send(database.users);
+	res.json('I am root');
 });
 app.post('/signin', (req, res) => {
 	// Load hash from your password DB.
-	bcrypt.compare("bacon", hash, function(err, res) {
-	    // res == true
-	});
-	bcrypt.compare("veggies", hash, function(err, res) {
-	    // res = false
-	});
-	if (req.body.email === database.users[0].email &&
-		req.body.password === database.users[0].password) {
-		res.json('Success!');
-	} else res.status(400).json('Error signing in');
+	const {email, password} = req.body;
+	console.log(email);
+	res.status(400).json(email);
 });
 app.post('/register', (req, res) => {
 	const {email, name, password} = req.body;
 	bcrypt.hash(password, null, null, function(err, hash) {
 	    if (err) throw err;
-	    console.log(hash);
 	});
-	database.users.push({
-		id: ++idCount,
-		name: name,
+	db('users')
+		.returning('*')
+		.insert({
 		email: email,
-		password: password,
-		entries: 0,
+		name: name,
 		joined: new Date()
+	})
+		.then(query => {
+		res.json(query);
+	})
+		.catch(err => {
+		res.status(400).json('unable to register');
 	});
-	res.json(database.users[database.users.length-1]);
 });
 app.get('/profile/:id', (req, res) => {
 	const {id} = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	});
-	if (!found) res.status(400).json(`user ${id} not found`);
+	// let found = false;
+	if (user) res.json(user);
+	else res.status(400).json(`user ${id} not found`);
 });
 app.put('/image', (req, res) => {
 	const {id} = req.params;
