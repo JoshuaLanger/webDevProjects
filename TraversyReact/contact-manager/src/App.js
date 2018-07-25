e2a6823afaa518632af8fc0d4ba98b4726a6e09a
Joshua Langer <joshuarllanger@gmail.com>
2018-07-25 08:29:07 -0500

    began contact-manager app

--- TraversyReact/contact-manager/src/App.js
+++ TraversyReact/contact-manager/src/App.js
@@ -1,23 +1,10 @@
 import React, { Component } from "react";
 
-import Header from "./components/Header";
-import Contact from "./components/Contact";
-
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header branding="Contact Manager" />
-        <Contact
-          name="BillyBob Jean"
-          phone="764-234-9875"
-          address="1600 pinness ave"
-        />
-        <Contact
-          name="Karen Billy"
-          phone="654-234-1234"
-          address="1601 pinness ave"
-        />
+      <div className="App">
+        <h1>I'm the app!</h1>
       </div>
     );
   }
